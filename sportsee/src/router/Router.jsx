a35b32bc4d275637334cf32dbab60d03990f184e
Alan62 <beaucheron.alan@free.fr>
2022-07-25 09:47:01 +0200

    datajson + router + modif App.js

--- sportsee/src/router/Router.jsx
+++ sportsee/src/router/Router.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-//import Dashboard from '../routes/Dashboard'
-//import UserSelect from '../routes/UserSelect'
-//import ErrorPage from '../routes/ErrorPage'
+import Dashboard from '../routes/Dashboard/Dashboard'
+import UserSelect from '../routes/UserSelect/UserSelect' 
+import ErrorPage from '../routes/ErrorPage/ErrorPage'  
+          
 
 const Router = () => {
   return (
@@ -11,7 +12,7 @@ const Router = () => {
         <Routes>
           <Route path="/" element={<UserSelect />} />
           <Route path="/:userId" element={<Dashboard />} />
-          <Route path="/*" element={<ErrorPage />} />
+          <Route path="/errorPage" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </div>
