5586a7ed9e94823f527b2db33a17b4e29edb3c53
Alan62 <beaucheron.alan@free.fr>
2022-07-26 02:12:54 +0200

    pages userselect

---
+++ sportsee/src/routes/UserSelect/UserSelect.jsx
@@ -0,0 +1,53 @@
+import { NavLink } from 'react-router-dom'
+import karl from '../../assets/images/karl.jpg'
+import cecilia from '../../assets/images/cecilia.jpg'
+import john from '../../assets/images/john.jpg'
+import './userselect.css'
+import TopNav from '../../components/TopNav/TopNav'
+import LeftNavBar from '../../components/LeftNavBar/LeftNavBar'
+/**
+ *
+ * @returns {ReactElement} login page
+ */
+const UserSelect = () => {
+  return (
+    <div className="userSelect">
+      <TopNav />
+      <LeftNavBar />
+      <div className="userContainer">
+        <ul className='ul__userSelect'>
+          <NavLink className="navlink" to="/12">
+            <div className="userCard">
+              <div className="imgContainer__userSelect">
+                <img className="img__userSelect" src={karl} alt="karl " />
+              </div>
+              <p className='p__userSelect'>Karl</p>
+              <p className='p__userSelect'>Dovineau</p>
+            </div>
+          </NavLink>
+
+          <NavLink className="navlink" to="/18">
+            <div className="userCard">
+              <div className="imgContainer__userSelect">
+                <img className="img__userSelect" src={cecilia} alt="cécilia " />
+              </div>
+              <p className='p__userSelect'>Cecilia</p>
+              <p className='p__userSelect'>Ratorez</p>
+            </div>
+          </NavLink>
+          <NavLink className="navlink" to="/errorPage">
+            <div className="userCard">
+              <div className="imgContainer__userSelect">
+                <img className="img__userSelect" src={john} alt="john " />
+              </div>
+              <p className='p__userSelect'>John</p>
+              <p className='p__userSelect'>Doe</p>
+            </div>
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default UserSelect
