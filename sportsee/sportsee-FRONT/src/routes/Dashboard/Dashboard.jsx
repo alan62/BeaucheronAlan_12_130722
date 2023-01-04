32f772a20851cdf0fda92c769424f81f6e56b980
Alan62 <beaucheron.alan@free.fr>
2023-01-04 21:36:38 +0100

    restructuration + add backend

--- sportsee/sportsee-FRONT/src/routes/Dashboard/Dashboard.jsx
+++ sportsee/sportsee-FRONT/src/routes/Dashboard/Dashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import Header from '../../components/Header/Header'
 import LeftNavBar from '../../components/LeftNavBar/LeftNavBar'
